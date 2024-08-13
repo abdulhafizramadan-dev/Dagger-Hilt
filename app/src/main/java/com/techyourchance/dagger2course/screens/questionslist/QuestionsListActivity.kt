446107e58ff4b55f6fd3668339125f8ef6019e16
Abdul Ramadan <abdul.ramadan@gojek.com>
2024-08-13 22:40:29 +0700

    Extraction of networking logic on QuestionsList screen

--- app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionsListActivity.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/questionslist/QuestionsListActivity.kt
@@ -2,40 +2,30 @@ package com.techyourchance.dagger2course.screens.questionslist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.techyourchance.dagger2course.Constants
-import com.techyourchance.dagger2course.networking.StackoverflowApi
+import com.techyourchance.dagger2course.questions.FetchQuestionsUseCase
 import com.techyourchance.dagger2course.questions.Question
 import com.techyourchance.dagger2course.screens.common.dialogs.ServerErrorDialogFragment
 import com.techyourchance.dagger2course.screens.questiondetails.QuestionDetailsActivity
-import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancelChildren
 import kotlinx.coroutines.launch
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener {
 
     private val coroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
-    private lateinit var stackoverflowApi: StackoverflowApi
-
     private lateinit var viewMvc: QuestionsListViewMvc
+    private lateinit var fetchQuestionsUseCase: FetchQuestionsUseCase
+
     private var isDataLoaded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewMvc = QuestionsListViewMvc(layoutInflater = layoutInflater, parent = null)
         setContentView(viewMvc.rootView)
-
-        // init retrofit
-        val retrofit = Retrofit.Builder()
-                .baseUrl(Constants.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        stackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+        fetchQuestionsUseCase = FetchQuestionsUseCase()
     }
 
     override fun onStart() {
@@ -64,17 +54,15 @@ class QuestionsListActivity : AppCompatActivity(), QuestionsListViewMvc.Listener
         coroutineScope.launch {
             viewMvc.showProgressIndication()
             try {
-                val response = stackoverflowApi.lastActiveQuestions(20)
-                if (response.isSuccessful && response.body() != null) {
-                    viewMvc.bindData(response.body()!!.questions)
-                    isDataLoaded = true
-                } else {
-                    onFetchFailed()
+                when(val result = fetchQuestionsUseCase.fetchLatestQuestions()) {
+                    is FetchQuestionsUseCase.Result.Success -> {
+                        viewMvc.bindData(result.questions)
+                        isDataLoaded = true
+                    }
+                    FetchQuestionsUseCase.Result.Failure -> onFetchFailed()
                 }
             } catch (t: Throwable) {
-                if (t !is CancellationException) {
-                    onFetchFailed()
-                }
+                onFetchFailed()
             } finally {
                 viewMvc.hideProgressIndication()
             }
