a63bfcda72c0a91f01a9fa11d9202d16a8df7106
Abdul Ramadan <abdul.ramadan@gojek.com>
2024-08-17 22:18:53 +0700

    Using qualifier annotations to differentiate between multiple services of the same type

--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/app/AppModule.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/app/AppModule.kt
@@ -2,7 +2,9 @@ package com.techyourchance.dagger2course.common.dependencyinjection.app
 
 import android.app.Application
 import com.techyourchance.dagger2course.Constants
+import com.techyourchance.dagger2course.common.dependencyinjection.Retrofit1
 import com.techyourchance.dagger2course.networking.StackoverflowApi
+import com.techyourchance.dagger2course.networking.UrlProvider
 import dagger.Module
 import dagger.Provides
 import retrofit2.Retrofit
@@ -12,18 +14,33 @@ import retrofit2.create
 @Module
 class AppModule(private val application: Application) {
 
+    @Retrofit1
     @AppScope
     @Provides
-    fun retrofit(): Retrofit {
+    fun retrofit1(urlProvider: UrlProvider): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
+            .baseUrl(urlProvider.getBaseUrl1())
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
     @AppScope
     @Provides
-    fun stackoverflowApi(retrofit: Retrofit): StackoverflowApi {
+    fun retrofit2(urlProvider: UrlProvider): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(urlProvider.getBaseUrl2())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun urlProvider(): UrlProvider {
+        return UrlProvider()
+    }
+
+    @AppScope
+    @Provides
+    fun stackoverflowApi(@Retrofit1 retrofit: Retrofit): StackoverflowApi {
         return retrofit.create()
     }
 
---
+++ app/src/main/java/com/techyourchance/dagger2course/networking/UrlProvider.kt
@@ -0,0 +1,13 @@
+package com.techyourchance.dagger2course.networking
+
+import com.techyourchance.dagger2course.Constants
+
+class UrlProvider {
+    fun getBaseUrl1(): String {
+        return Constants.BASE_URL
+    }
+
+    fun getBaseUrl2(): String {
+        return "other_base_url"
+    }
+}
