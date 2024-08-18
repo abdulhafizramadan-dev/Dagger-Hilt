c256c92b71c91424f423fe0258320f1928761111
Abdul Ramadan <abdul.ramadan@gojek.com>
2024-08-19 01:22:08 +0700

    Refactoring Dagger's modules according to Hilt's conventions

--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/app/AppScope.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/app/AppScope.kt
@@ -1,6 +1,9 @@
 package com.techyourchance.dagger2course.common.dependencyinjection.app
 
+import dagger.hilt.migration.AliasOf
 import javax.inject.Scope
+import javax.inject.Singleton
 
 @Scope
+@AliasOf(Singleton::class)
 annotation class AppScope
